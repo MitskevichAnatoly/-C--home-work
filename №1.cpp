0ad9076603be05323b4cbd820b7a63571a60a670
Мицкевич Анатолий <43838456+MitskevichAnatoly@users.noreply.github.com>
2018-11-25 22:23:56 +0300

    Add files via upload

---
+++ №1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int x=1;x<=38892;x++)
+    {
+        int y;
+    if((78487894-2018*x)%2019==0)
+    {
+        y=(78487894-2018*x)%2019;
+        cout<<"("<<x<<";"<<y<<")"<<endl;
+    }
+
+    }
+
+    return 0;
+}
